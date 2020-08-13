7d0d900e3ba1a0f485617ab9fcb9cc05df731ae5
Robert May <robertmay@Roberts-MBP.fios-router.home>
2020-08-13 07:25:16 -0400

    Birds & such

---
+++ Assets/Scripts/BirdSpawner.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<lb_BirdController>().SpawnAmount(10);
+    }
+}
